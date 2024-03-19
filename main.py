8d29a69d7254c817ba55a6edfe92fff79b173e65
George <serfertor@mail.ru>
2024-03-19 23:12:30 +0300

    Try before switching to MNIST Dataset

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from keras.src.callbacks import EarlyStopping
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+import PIL
+from PIL import Image
 
 
 def get_digits(filepath):
@@ -63,7 +65,10 @@ def get_digits(filepath):
     # Обрезка отдельных цифр
     for i in corners:
         digits.append(cv2.bitwise_not(morph[i[0][1]:i[1][1], i[0][0]:i[1][0]]))
-
+    j = 0
+    for i in digits:
+        cv2.imwrite(f"{j}.jpg", i)
+        j += 1
     return digits
 
 
@@ -71,16 +76,12 @@ def train_model():
     classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     train_data = tf.keras.utils.image_dataset_from_directory("Resources/dataset/train",
                                                              class_names=classes,
-                                                             validation_split=0.2,
-                                                             subset="training",
                                                              seed=123,
                                                              label_mode='categorical',
                                                              color_mode="grayscale",
                                                              image_size=(64, 64))
-    val_data = tf.keras.utils.image_dataset_from_directory("Resources/dataset/train",
+    val_data = tf.keras.utils.image_dataset_from_directory("Resources/dataset/validation",
                                                            class_names=classes,
-                                                           validation_split=0.2,
-                                                           subset="validation",
                                                            seed=123,
                                                            label_mode='categorical',
                                                            color_mode="grayscale",
@@ -91,8 +92,8 @@ def train_model():
         layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
         layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),  #
-        layers.Flatten(),  #
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
         layers.Dense(len(classes), activation="softmax")])
 
     model.compile(
@@ -104,25 +105,62 @@ def train_model():
     #     monitor='val_accuracy',
     #     patience=3,
     #     min_delta=0.01,
-    #     restore_best_weights=True  # Использование лучших весов модели
+    #     restore_best_weights=True
     # )
     model.fit(
         train_data,
         validation_data=val_data,
-        epochs=15)
-    # callbacks=[callback])  # доля данных, отданных на проверку
-
-    score = model.evaluate(train_data, verbose=0)  # оцениваем нашу модель (verbose=0 подавляет вывод на экран)
-    print("Test loss:", score[0])  # функция ошибки на тестовых данных
-    print("Test accuracy:", score[1])  # метрика (из заданных, у нас accuracy) на тестовых данных
+        epochs=10)
+    # callbacks=[callback])
 
     model.save("model.h5")
     model.save("model.keras")
 
 
+def recognize_time(pathfile, model):
+    num = get_digits(pathfile)
+    digit = num[3]
+    digit = cv2.resize(digit, (64, 64), interpolation=cv2.INTER_LINEAR)
+    digit = tf.keras.utils.img_to_array(digit)
+    digit /= 255.0
+    digit = tf.expand_dims(digit, 0)  # Create a batch
+
+    print(digit.shape)
+    print(digit)
+    print(np.argmax(tf.nn.softmax(model.predict(digit)[0])))
+
+
 if __name__ == '__main__':
     # image = cv2.imread('Resources/AI - RTUITLab/Photo/00_08.jpg')
-    # # image = cv2.imread('Resources/AI - RTUITLab/Random Photos/01_05.jpg')
-    model = tf.load_model("model.keras")
-    while True:
-        print("Введите 1 - для ввода пути до файла вручную")
+    # image = cv2.imread('Resources/AI - RTUITLab/Random Photos/01_05.jpg')
+    model = keras.models.load_model("model.keras")
+    get_digits("Resources/AI - RTUITLab/Photo/01_42.jpg")
+
+
+    img = tf.keras.utils.load_img(
+        "1.jpg", target_size=(64, 64), color_mode="grayscale"
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+
+    print(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"][np.argmax(score)], 100 * np.max(score))
+    )
+    # while True:
+    #     print("Введите 1 - для ввода пути до файла вручную \n"
+    #           "Введите 2 - для ввода пути до директории \n"
+    #           "Введите 0 - для выхода из программы")
+    #     text = input()
+    #     match text:
+    #         case '0':
+    #             break
+    #         case '1':
+    #             print ("Введите путь до файла")
+    #             path = input()
+    #             recognize_time(path, model)
+    #         case _:
+    #             continue
